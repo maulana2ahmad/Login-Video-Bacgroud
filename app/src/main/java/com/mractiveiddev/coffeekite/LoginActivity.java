fea4069bbd201ae1c96af56d4fe35bdba2b0c91f
maulana2ahmad <maulana2ahmad@gmail.com>
2019-02-19 20:39:14 +0700

    Login video backgroud

---
+++ app/src/main/java/com/mractiveiddev/coffeekite/LoginActivity.java
@@ -0,0 +1,50 @@
+package com.mractiveiddev.coffeekite;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.VideoView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    VideoView videoBG;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+
+        videoBG = (VideoView) findViewById(R.id.videoBG);
+
+        Uri uri = Uri.parse("android.resource://"
+                + getPackageName()
+                + "/"
+                + R.raw.river);
+
+        videoBG.setVideoURI(uri);
+        videoBG.start();
+
+        videoBG.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setLooping(true);
+            }
+        });
+
+
+    }
+
+    public void OnbtLogIn(View view) {
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(i);
+    }
+}
